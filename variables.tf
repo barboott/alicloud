aec0ad117a1063bb55e619b27ba182d7bdfc6993
Barboott <jiandong.wang@outlook.com>
2022-12-01 16:27:17 +0800

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "alicloud_access_key" {
+  type = string
+}
+
+variable "alicloud_secret_key" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
---
+++ version.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "1.193.0"
+    }
+  }
+}
+
+provider "alicloud" {
+  # Configuration options
+}
